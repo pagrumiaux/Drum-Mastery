3b6245aa7fb79ed2fcd4329004b08223957be1bc
Peehay <pierreamaury.grumiaux@gmail.com>
2015-10-21 16:20:18 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <QApplication>
+#include <QScrollArea>
 
 #include "sheet.h"
 #include "note.h"
@@ -8,9 +9,24 @@ int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
 
-    Sheet sheet(0);
+    Sheet *sheet = new Sheet(0);
+    sheet->setFixedWidth(500);
+    sheet->setMinimumHeight(1000);
+    QScrollArea *scrollArea = new QScrollArea;
+
+    scrollArea->setWidget(sheet);
+    scrollArea->setFixedWidth(500);
+    scrollArea->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+    scrollArea->ensureVisible(0, 500);
+
+    sheet->setFocus();
+    sheet->show();
+    scrollArea->show();
+
+
+    /*Sheet sheet(0);
     sheet.setFixedSize(500,250);
-    sheet.show();
+    sheet.show();*/
 
     return app.exec();
 }
