e16f88d0fa1be318b780297c2c2e4c9f8dba4314
Peehay <pierreamaury.grumiaux@gmail.com>
2015-10-30 17:05:15 +0100

    Update sheet.h

--- sheet.h
+++ sheet.h
@@ -9,6 +9,7 @@
 #include <QTime>
 #include <QMediaPlayer>
 #include <QScrollArea>
+#include <QPixmap>
 
 #include "measure.h"
 #include "notes_jugees.h"
@@ -22,6 +23,8 @@ public:
     Note_jugee* getNoteJugee(int numero, Note_jugee* premiere_note);
     void resetListeJ(Note_jugee* premiere_note);
     int getIndice_essai();
+    int getPerformance();
+    QPixmap ImageOfNote(Note *note, char c);
 
 public slots:
 
@@ -31,22 +34,27 @@ protected:
     void keyPressEvent(QKeyEvent *event);
 
 private:
-    QBasicTimer timer;
+    QBasicTimer timer; // timer
+    QTime time; // timer
     int position;
     int step;
-    QTime time;
-    int vitesse;
+    int vitesse; //vitesse de l'update
     int bpm; // battement par minute (tempo)
-    bool vue;
-    bool started;
+
+    bool vue; //vrai si on veut avoir la vue
+    bool started; //vrai si on est en train de jouer
+    bool decompte; //vrai pour le décompte, faux une fois le décompte effectué
+
     QMediaPlayer kick;
     QMediaPlayer metronome;
+
+    int performance; //indice de performance
 
     int m_taille_mesure; // taille d'une mesure en pixel
     Measure m_mesure;
 
-    Note_jugee* tab_liste_jugee[10];
-    int indice_j;
-    int indice_essai;
+    Note_jugee* tab_liste_jugee[10]; //tableau qui retient les notes jouées (jusqu'à 10 essais)
+    int indice_j; //indice pour parcourir une liste de notes jugées
+    int indice_essai; //indice pour parcourir le tableau des listes de notes jugées
 };
 
