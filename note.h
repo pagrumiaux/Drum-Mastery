b2d6b8d002cb8b74cecb1091693f8ed9d325f04f
Peehay <pierreamaury.grumiaux@gmail.com>
2015-12-23 14:15:59 +0100

    Update note.h

--- note.h
+++ note.h
@@ -5,13 +5,13 @@ class Note
 {
 public:
     Note();
-    Note(int type, float valeur);
-    Note(int type, float valeur, Note *suivante);
+    Note(int type, double valeur);
+    Note(int type, double valeur, Note *suivante);
 
     void ajouter_suivante(Note *suivante); // ajoute une note a la liste
     void suppr_suivantes(); //supprime toutes les notes suivantes dans la liste (dans la mesure)
 
-    float getValeur(); //renvoie la valeur de la note (dépend du chiffrage)
+    double getValeur(); //renvoie la valeur de la note (dépend du chiffrage)
     int getType(); //renvoie le type de note
     Note* getSuivante(); //renvoie un pointeur vers la note suivante dans la liste chainee
     bool estDerniere(); //renvoie true ou false si elle est derniere ou pas dans la liste chainee
@@ -19,7 +19,7 @@ public:
 private:
     Note *m_suivante; //pointeur vers la prochaine note dans la liste chainée
     int m_type; //0 pour un soupir, 1 pour une note
-    float m_valeur; //valeur de la note
+    double m_valeur; //valeur de la note
     int m_numero; //numero de la note
     bool derniere; //derniere note ou pas
 
