bc988c0b9e86b94fe4a518bf1b5106f4c658edfb
ysmahi <yazidsmahi@gmail.com>
2016-01-12 13:55:30 +0100

    Update boum.h

--- boum.h
+++ boum.h
@@ -1,18 +1,30 @@
 #ifndef BOUM_H
 #define BOUM_H
 
+#include <QObject>
+#include "portaudio.h"
+#include "fenentrainement.h"
 
-class Boum
-{
-public:
-    Boum();
-    int fonctionBoum(void);
-};
+class Boum : public QObject
+       {
+    Q_OBJECT
+
+    public:
+        Boum();
+        int fonctionBoum(void);
+
+    public slots :
+        void doWork ();
+
+    signals :
+        void resultReady ();
+    };
 
 static int recordCallback(const void *inputBuffer, void *outputBuffer,
-    unsigned long framesPerBuffer,
-    const PaStreamCallbackTimeInfo* timeInfo,
-    PaStreamCallbackFlags statusFlags,
-    void *userData);
+      unsigned long framesPerBuffer,
+      const PaStreamCallbackTimeInfo* timeInfo,
+      PaStreamCallbackFlags statusFlags,
+      void *userData);
 
 #endif // BOUM_H
+
