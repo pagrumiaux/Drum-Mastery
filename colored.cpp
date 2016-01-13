cfb6285c0b98eca2f1db69a97f149fc81ba81a6a
Peehay <pierreamaury.grumiaux@gmail.com>
2016-01-13 14:49:37 +0100

    Update colored.cpp

--- colored.cpp
+++ colored.cpp
@@ -9,10 +9,10 @@
 #include "math.h"
 #include "colored.h"
 
-Colored::Colored(QWidget *parent, int taille_mesure, Note_jugee* t_liste_jugee[10], Measure *mesure, int vit, int nb_mesure)
+Colored::Colored(QWidget *parent, int taille_mesure, Note_jugee* t_liste_jugee[10], Measure *mesure, int vit, int nb_mesure, int dureeTemps, int dureeMesure)
     : QWidget(parent)
 {
-    vue = false;
+    vue = true;
     m_taille_mesure = taille_mesure;
     nbre_mesure = nb_mesure;
     int i;
@@ -21,6 +21,8 @@ Colored::Colored(QWidget *parent, int taille_mesure, Note_jugee* t_liste_jugee[1
     m_mesure = mesure;
     vitesse = vit;
     indice_essai = 1;
+    duree1Temps = dureeTemps;
+    duree1Mesure = dureeMesure;
 }
 
 
@@ -125,32 +127,49 @@ void Colored::paintEvent(QPaintEvent * /* event */)
             QFont font = painter.font();
             font.setPointSize(25);
             painter.setFont(font);
-            painter.drawText(15, 95+a*75, p);
+            if(nbre_mesure <= 2)
+                painter.drawText(15, 95+a*75, p);
+            else
+                painter.drawText(15, 95+a*125, p);
         }
     }
 
     /*Dessin sur la mesure d'essai*/
     QRectF source(0, 0, 30, 58);
-    if(vue)
+    if(vue && !started && indice_essai >= 10)
     {
-        int b;
-        for (b=0; b<indice_essai; b++)
+        painter.setPen(QPen(Qt::black, 10));
+        int a;
+        for(a=0; a<indice_essai; a++)
         {
-            painter.setPen(QPen(Qt::black, 10));
-            int x = 50;
-            Note *note_int = m_mesure->getNote(); //note intermédiaire pour parcourir la liste de notes
-            float valeur_prec = note_int->getValeur(); //retient en mémoire la valeur de la note d'avant
-            painter.drawPixmap(QRectF(x, 13+75*b, 30, 58), QPixmap("C:/Users/P-A/Documents/Drum Mastery/Images/Notes/noire_g.png"), source);
-
-            while (note_int->estDerniere() != true)
+            int x = 53;
+            if(nbre_mesure == 1)
+                x = 303;
+            int j;
+            Measure* mesure_int = m_mesure;
+            for(j=0; j < nbre_mesure; j++)
             {
-                x += valeur_prec*m_taille_mesure;
-                note_int = note_int->getSuivante();
-                painter.drawPixmap(QRectF(x, 13+75*b, 30, 58), ImageOfNote(note_int, 'g'), source);
-                valeur_prec = note_int->getValeur();
+                if (j == 2)
+                    x = 53;
+                if(mesure_int->getNote() != NULL)
+                {
+                    Note *note_int = mesure_int->getNote(); //note intermédiaire pour parcourir la liste de notes
+                    float valeur_prec = note_int->getValeur(); //retient en mémoire la valeur de la note d'avant
+                    while (note_int->estDerniere() != true)
+                    {
+                        note_int = note_int->getSuivante();
+                        if(nbre_mesure <= 2)
+                            painter.drawPixmap(QRectF(x, (j >= 2)? 58 + a*75 : 8 + a*75, 30, 58), ImageOfNote(note_int, 'g'), source);
+                        else
+                            painter.drawPixmap(QRectF(x, (j >= 2)? 58 + a*125 : 8 + a*125, 30, 58), ImageOfNote(note_int, 'g'), source);
+                        valeur_prec = note_int->getValeur();
+                        x += int(valeur_prec*m_taille_mesure);
+                    }
+                }
+                mesure_int = mesure_int->getSuivante();
             }
         }
-    }
+     }
 
     /*écriture et coloration des notes jouées sur la mesure d'essais*/
     int c;
@@ -167,14 +186,18 @@ void Colored::paintEvent(QPaintEvent * /* event */)
             Note_jugee *liste_note = tab_liste_jugee[c];
             while (!liste_note->estDerniere())
             {
-                if ((liste_note->getTemps()+vitesse*16 > 2000*nbre_mesure))
+                if ((liste_note->getTemps()+vitesse*16 > duree1Mesure*nbre_mesure))
                 {
-                    xn = liste_note->getTemps()/vitesse+50 + 250 - m_taille_mesure;
+                    xn = 50 + 250 + liste_note->getTemps()*m_taille_mesure/duree1Mesure - m_taille_mesure;
+                    //qDebug("temps : %d", liste_note->getTemps());
+                    //qDebug("xn : %d\n", xn);
                     yn = 13 + c*75;
                 }
                 else
                 {
-                    xn = liste_note->getTemps()/vitesse+50 + 250;
+                    xn = liste_note->getTemps()*m_taille_mesure/duree1Mesure+50 + 250;
+                    //qDebug("temps : %d", liste_note->getTemps());
+                    //qDebug("xn : %d\n", xn);
                     yn = 13 + c*75;
                 }
 
@@ -192,14 +215,14 @@ void Colored::paintEvent(QPaintEvent * /* event */)
             }
             if(!liste_note->estDefaut())
             {
-                if ((liste_note->getTemps()+vitesse*16 > 2000*nbre_mesure))
+                if ((liste_note->getTemps()+vitesse*16 > duree1Mesure*nbre_mesure))
                 {
-                    xn = liste_note->getTemps()/vitesse+50 + 250 - m_taille_mesure;
+                    xn = liste_note->getTemps()*m_taille_mesure/duree1Mesure + 50 + 250 - m_taille_mesure;
                     yn = 13 + c*75;
                 }
                 else
                 {
-                    xn = liste_note->getTemps()/vitesse+50 + 250;
+                    xn = liste_note->getTemps()*m_taille_mesure/duree1Mesure + 50 + 250;
                     yn = 13 + c*75;
                 }
 
@@ -213,7 +236,7 @@ void Colored::paintEvent(QPaintEvent * /* event */)
                     //qDebug("xn : %d, temps : %d, essai : %d", xn, liste_note->getTemps(), c+1);
                     painter.drawPixmap(QRectF(xn, yn, 30, 58), image2, source2); //dessin de la derniere note jugee
                 }
-            }            
+            }
         }
         break;
 
@@ -223,19 +246,19 @@ void Colored::paintEvent(QPaintEvent * /* event */)
             Note_jugee *liste_note = tab_liste_jugee[c];
             while (!liste_note->estDerniere())
             {
-                if(c == 0 && liste_note == getNoteJugee(1, tab_liste_jugee[0]) && liste_note->getTemps()+vitesse*16 > 2000)
+                if(c == 0 && liste_note == getNoteJugee(1, tab_liste_jugee[0]) && liste_note->getTemps()+vitesse*16 > duree1Mesure)
                 {
-                    xn = liste_note->getTemps()/vitesse+50 - m_taille_mesure;
+                    xn = liste_note->getTemps()*m_taille_mesure/duree1Mesure + 50 - m_taille_mesure;
                     yn = 13 + c*75;
                 }
-                else if ((liste_note->getTemps()+vitesse*16 > 2000*nbre_mesure))
+                else if ((liste_note->getTemps()+vitesse*16 > duree1Mesure*nbre_mesure))
                 {
-                    xn = liste_note->getTemps()/vitesse+50 - 2*m_taille_mesure;
+                    xn = liste_note->getTemps()*m_taille_mesure/duree1Mesure + 50 - 2*m_taille_mesure;
                     yn = 13 + c*75;
                 }
                 else
                 {
-                    xn = liste_note->getTemps()/vitesse+50;
+                    xn = liste_note->getTemps()*m_taille_mesure/duree1Mesure + 50;
                     yn = 13 + c*75;
                 }
 
@@ -246,26 +269,25 @@ void Colored::paintEvent(QPaintEvent * /* event */)
 
                 if (!((liste_note->getTemps() == 0) && (liste_note->getSuivante() == NULL) && (liste_note->getCorrecte() == false)))
                 {
-                    qDebug("xn : %d, yn : %d, temps : %d", xn, yn, liste_note->getTemps());
                     painter.drawPixmap(QRectF(xn, yn, 30, 58), image2, source2);
                 }
                 liste_note = liste_note->getSuivante();
             }
             if(!liste_note->estDefaut())
             {
-                if(liste_note == getNoteJugee(1, tab_liste_jugee[0]) && liste_note->getTemps()+vitesse*16 > 2000*nbre_mesure)
+                if(liste_note == getNoteJugee(1, tab_liste_jugee[0]) && liste_note->getTemps()+vitesse*16 > duree1Mesure)
                 {
-                    xn = liste_note->getTemps()/vitesse+50 - m_taille_mesure;
+                    xn = liste_note->getTemps()*m_taille_mesure/duree1Mesure + 50 - m_taille_mesure;
                     yn = 13 + c*75;
                 }
-                else if ((liste_note->getTemps()+vitesse*16 > 2000*nbre_mesure))
+                else if ((liste_note->getTemps()+vitesse*16 > duree1Mesure*nbre_mesure))
                 {
-                    xn = liste_note->getTemps()/vitesse+50 - 2*m_taille_mesure;
+                    xn = liste_note->getTemps()*m_taille_mesure/duree1Mesure + 50 - 2*m_taille_mesure;
                     yn = 13 + c*75;
                 }
                 else
                 {
-                    xn = liste_note->getTemps()/vitesse+50;
+                    xn = liste_note->getTemps()*m_taille_mesure/duree1Mesure + 50;
                     yn = 13 + c*75;
                 }
 
@@ -276,7 +298,6 @@ void Colored::paintEvent(QPaintEvent * /* event */)
 
                 if (!((liste_note->getTemps() == 0) && (liste_note->getSuivante() == NULL) && (liste_note->getCorrecte() == false)))
                 {
-                    qDebug("xn : %d, yn : %d, temps : %d", xn, yn, liste_note->getTemps());
                     painter.drawPixmap(QRectF(xn, yn, 30, 58), image2, source2); //dessin de la derniere note jugee
                 }
             }
@@ -289,21 +310,25 @@ void Colored::paintEvent(QPaintEvent * /* event */)
             Note_jugee *liste_note = tab_liste_jugee[c];
             while (!liste_note->estDerniere())
             {
-                if ((liste_note->getTemps()+vitesse*16 > 2000*nbre_mesure))
+                if(liste_note == getNoteJugee(1, tab_liste_jugee[0]) && liste_note->getTemps()+vitesse*16 > duree1Mesure)
                 {
-                    xn = liste_note->getTemps()/vitesse+50 - 3*m_taille_mesure;
+                    xn = liste_note->getTemps()*m_taille_mesure/duree1Mesure + 50 - m_taille_mesure;
+                    yn = 13 + c*125;
+                }
+                else if ((liste_note->getTemps()+vitesse*16 > duree1Mesure*nbre_mesure))
+                {
+                    xn = liste_note->getTemps()*m_taille_mesure/duree1Mesure + 50 - 3*m_taille_mesure;
                     yn = 13 + c*125;
                 }
                 else
                 {
-                    xn = liste_note->getTemps()/vitesse+50;
+                    xn = liste_note->getTemps()*m_taille_mesure/duree1Mesure + 50;
                     yn = 13 + c*125;
                     if(xn > 2*m_taille_mesure)
                     {
                         xn = xn - 2*m_taille_mesure;
                         yn = yn + 50;
                     }
-
                 }
 
                 if(liste_note->getCorrecte())
@@ -320,14 +345,22 @@ void Colored::paintEvent(QPaintEvent * /* event */)
             }
             if(!liste_note->estDefaut())
             {
-                if ((liste_note->getTemps()+vitesse*16 > 2000*nbre_mesure))
+                if(liste_note == getNoteJugee(1, tab_liste_jugee[c]) && liste_note->getTemps()+vitesse*16 > duree1Mesure)
                 {
-                    xn = liste_note->getTemps()/vitesse+50 - 2*m_taille_mesure;
+                    if(c == 0)
+                        xn = liste_note->getTemps()*m_taille_mesure/duree1Mesure + 50 - m_taille_mesure;
+                    else
+                        xn = liste_note->getTemps()*m_taille_mesure/duree1Mesure + 50 - 3* m_taille_mesure;
+                    yn = 13 + c*125;
+                }
+                else if ((liste_note->getTemps()+vitesse*16 > duree1Mesure*nbre_mesure))
+                {
+                    xn = liste_note->getTemps()*m_taille_mesure/duree1Mesure + 50 - 2*m_taille_mesure;
                     yn = 13 + c*125;
                 }
                 else
                 {
-                    xn = liste_note->getTemps()/vitesse+50;
+                    xn = liste_note->getTemps()*m_taille_mesure/duree1Mesure + 50;
                     yn = 13 + c*125;
                     if(xn > 2*m_taille_mesure)
                     {
@@ -356,14 +389,19 @@ void Colored::paintEvent(QPaintEvent * /* event */)
             Note_jugee *liste_note = tab_liste_jugee[c];
             while (!liste_note->estDerniere())
             {
-                if ((liste_note->getTemps()+vitesse*16 > 2000*nbre_mesure))
+                if(liste_note == getNoteJugee(1, tab_liste_jugee[0]) && liste_note->getTemps()+vitesse*16 > duree1Mesure)
                 {
-                    xn = liste_note->getTemps()/vitesse+50 - 4*m_taille_mesure;
+                    xn = liste_note->getTemps()*m_taille_mesure/duree1Mesure + 50 - m_taille_mesure;
+                    yn = 13 + c*125;
+                }
+                else if ((liste_note->getTemps()+vitesse*16 > duree1Mesure*nbre_mesure))
+                {
+                    xn = liste_note->getTemps()*m_taille_mesure/duree1Mesure + 50 - 4*m_taille_mesure;
                     yn = 13 + c*125;
                 }
                 else
                 {
-                    xn = liste_note->getTemps()/vitesse+50;
+                    xn = liste_note->getTemps()*m_taille_mesure/duree1Mesure + 50;
                     yn = 13 + c*125;
                     if(xn > 2*m_taille_mesure)
                     {
@@ -386,14 +424,22 @@ void Colored::paintEvent(QPaintEvent * /* event */)
             }
             if(!liste_note->estDefaut())
             {
-                if ((liste_note->getTemps()+vitesse*16 > 2000*nbre_mesure))
+                if(liste_note == getNoteJugee(1, tab_liste_jugee[c]) && liste_note->getTemps()+vitesse*16 > duree1Mesure)
                 {
-                    xn = liste_note->getTemps()/vitesse+50 - 4*m_taille_mesure;
+                    if(c == 0)
+                        xn = liste_note->getTemps()*m_taille_mesure/duree1Mesure + 50 - m_taille_mesure;
+                    else
+                        xn = liste_note->getTemps()*m_taille_mesure/duree1Mesure + 50 - 4*m_taille_mesure;
+                    yn = 13 + c*125;
+                }
+                else if ((liste_note->getTemps()+vitesse*16 > duree1Mesure*nbre_mesure))
+                {
+                    xn = liste_note->getTemps()*m_taille_mesure/duree1Mesure + 50 - 4*m_taille_mesure;
                     yn = 13 + c*125;
                 }
                 else
                 {
-                    xn = liste_note->getTemps()/vitesse+50;
+                    xn = liste_note->getTemps()*m_taille_mesure/duree1Mesure + 50;
                     yn = 13 + c*125;
                     if(xn > 2*m_taille_mesure)
                     {
